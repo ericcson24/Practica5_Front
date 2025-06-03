5b840d2ffc1e3f9d172f9940c3b73c5cf48759b0
ericcson24 <150656730+ericcson24@users.noreply.github.com>
2025-06-03 18:44:45 +0200

    Delete Practica5_FRONT directory

--- Practica5_FRONT/components/MensajeBienvenida.tsx
+++
@@ -1,7 +0,0 @@
-interface Props {
-  nombre: string;
-}
-
-export default function MensajeBienvenida({ nombre }: Props) {
-  return <h1>Hola, {nombre} 👋</h1>;
-}
--- Practica5_FRONT/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-   return (
-    <div style="text-align: center; margin-top: 2rem;">
-      <h1>Bienvenido a la Nube</h1>
-      <a href="/profile">Ir al perfil</a>
-    </div>
-  );
-}
